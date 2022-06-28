5c303ded7aa0576cfc61b6f358836c1476b10504
mary leddy <maryleddy1@gmail.com>
2022-06-27 20:48:35 -0700

    ll

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+    render() {
 
-    return (
-      <header id="home">
+        return (
+            <header id="home">
+                <div className="two columns bannerSection">
+                    <div className="verticalNames">
+                        <br />
+                        <h1 className="verticalNames">M</h1>
+                        <h1 className="verticalNames">E</h1>
+                        <h1 className="verticalNames">G</h1>
+                        <br />
+                        <h1 className="verticalNames">&</h1>
+                        <br />
+                        <h1 className="verticalNames">M</h1>
+                        <h1 className="verticalNames">A</h1>
+                        <h1 className="verticalNames">R</h1>
+                        <h1 className="verticalNames">Y</h1>
+                    </div>
+                    <nav id="nav-wrap">
 
-      <nav id="nav-wrap">
+                        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                        <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+                        <ul id="nav" className="nav">
+                            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                            <li><a className="smoothscroll" href="#about">Our Story</a></li>
+                            <li><a className="smoothscroll" href="#calendar">Itinerary</a></li>
+                            <li><a className="smoothscroll" href="#portfolio">Recommendations</a></li>
+                            <li><a className="smoothscroll" href="#announcements">Registry</a></li>
+                            <li><a className="smoothscroll" href="#contact">RSVP</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="two columns bannerSection2">
+                    <div className="row banner">
+                        <div className="banner-text">
+                            <h1 className="responsive-headline">Levey Wedding</h1>
+                            <br></br>
+                        </div>
+                    </div>
+                </div>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#calendar">Calendar</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Resources</a></li>
-            <li><a className="smoothscroll" href="#announcements">Announcements</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">Post Academic Careers </h1>
-            <br></br>
-         </div>
-      </div>
-
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
-    );
-  }
+            </header>
+        );
+    }
 }
 
 export default Header;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+        if (this.props.data) {
+            var projects = this.props.data.projects.map(function (projects) {
+                var projectImage = 'images/portfolio/' + projects.image;
+                var projectIcon = 'images/portfolio/' + projects.category;
+                return <div key={projects.title} className="columns portfolio-item">
+
+                    <img alt={projects.title} src={projectIcon} className="reccomendationIcon" />
+                    <div className="item-wrap">
+                        <a href={projects.url} title={projects.title}>
+                            <img alt={projects.title} src={projectImage} />
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h6>{projects.title}</h6>
+                                    <h6>{projects.type}</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-      })
-    }
+            })
+        }
 
-    return (
-      <section id="portfolio">
-      <div className="row">
-         <div className="twelve columns collapsed">
-            <h1>These Are Some Useful Resources curated and written by our members</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
-            </div>
-          </div>
-      </div>
-   </section>
-    );
-  }
+        return (
+            <section id="portfolio">
+                <div className="row">
+                    <div className="twelve columns collapsed">
+                        <h1>Recommendations</h1>
+                        <hr id="portfolioHr" />
+                        <h2>Here are some recommendations for our out of town guests</h2>
+                        <div id="portfolio-wrapper" className="bgrid-sixths s-bgrid-thirds cf">
+                            {projects}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Portfolio;
